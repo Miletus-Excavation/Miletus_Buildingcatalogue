9ccb9b6f68f516bcaa2858fe7300133334b4ee09
Lisa Steinmann <lisa.steinmann@dainst.de>
2025-04-28 15:05:26 +0200

    update polygon and layer generation for map

--- R/duplicate_drawings.R
+++ R/duplicate_drawings.R
@@ -1,6 +1,5 @@
 library(idaifieldR)
-library(tidyverse)
-library(rgdal)
+library(dplyr)
 library(sf)
 library(utf8)
 
@@ -8,10 +7,10 @@ library(utf8)
 source(file = "R/functions.R")
 source(file = "R/load_data.R")
 
+##### load all polygons 
+Miletus_geom <- st_read(dsn = "SHP/Milet_gesamt_Polygone.shp") %>%
+  select(UUID, geometry)
 
-
-##### load paulas polygons 
-Miletus_geom <- st_read(dsn = "import/Miletus_geom.geojson")
 # add column to mark existing drawings
 Miletus_geom$hasGeom <- "Umzeichnung vorhanden"
 
@@ -20,28 +19,37 @@ Miletus_geom$hasGeom <- "Umzeichnung vorhanden"
 Miletus_geom <- st_zm(Miletus_geom, drop = TRUE, what = "ZM")
 
 #result
-Miletus_geom
+plot(Miletus_geom)
 
-# change "Name" variable to "identifier" to suit DB export
-colnames(Miletus_geom)[which(colnames(Miletus_geom) == "Name")] <- "identifier"
+# check for duplicate UUIDs
+Miletus_geom[which(table(Miletus_geom$UUID) > 1),]
 
 # make a to-do-list with DB-entries and polygons, full join to get both 
 # data frame complete and nots (mapWorkflow = "Fertig für Karte?")
 buildings_check <- data_mat %>%
   mutate(hasDB = "Datensatz vorhanden") %>%
-  select(identifier, hasDB, mapWorkflow, period.start, period.end) %>%
+  select(identifier, id, hasDB, mapWorkflow, period.start, period.end) %>%
+  rename(UUID = id) %>%
   full_join(Miletus_geom) %>%
   select(identifier, hasDB, hasGeom, mapWorkflow, period.start, period.end)
 
 # save it for later
-write.csv(buildings_check, file = "export/currentGeometryDB.csv", 
+write.csv(buildings_check, file = "R/export/currentStateDB.csv", 
           fileEncoding = "UTF-8", na = "")
 # remove, because we only needed the csv
 rm(buildings_check)
 
 # get the identifier and dating / periods of each building (data_mat is made in file "R/load_data.R")
 buildings_dating <- data_mat %>% 
-  select(identifier, context, buildingType, period.start, period.end)
+  select(identifier, id, context, buildingType, period.start, period.end, arachneId, gazId) %>%
+  rename(UUID = id) %>%
+  mutate(arachneId = ifelse(is.na(arachneId), NA,
+                            paste0("https://arachne.dainst.org/entity/", arachneId))) %>%
+  mutate(gazId = ifelse(is.na(gazId), NA, 
+                        paste0("https://gazetteer.dainst.org/place/", gazId))) %>%
+  mutate(URL = ifelse(is.na(arachneId), gazId, arachneId)) %>%
+  mutate(URL = ifelse(is.na(URL), "https://www.miletgrabung.uni-hamburg.de/milet-tour.html", URL)) %>% 
+  select(-arachneId, -gazId)
 
 
 # function to make the loop cleaner
@@ -106,7 +114,7 @@ replace_by_period_group <- function(single_building, periods_groups = periods_gr
 }
 
 # set up a new df in which the duplicated rows will go
-buildings_dating_dupl <- as.data.frame(matrix(ncol = ncol(buildings_dating)+1, nrow = 1))
+buildings_dating_dupl <- as.data.frame(matrix(ncol = ncol(buildings_dating) + 1, nrow = 1))
 colnames(buildings_dating_dupl) <- c(colnames(buildings_dating), "period")
 
 # replace and duplicate each building, appending the result to the new df
@@ -117,7 +125,7 @@ for(i in 1:nrow(buildings_dating)) {
 
 # remove the first row (NAs) and set the rownames for all rows
 buildings_dating_dupl <- buildings_dating_dupl[-1,]
-rownames(buildings_dating_dupl) <- buildings_dating_dupl$identifier
+# no: rownames(buildings_dating_dupl) <- buildings_dating_dupl$identifier
 # never mind the error!
 
 # merge everything with left joins for period (only adding the corresponging values)
@@ -128,7 +136,7 @@ buildings_complete <- buildings_dating_dupl %>%
   left_join(periods_groups) %>%
   full_join(Miletus_geom) %>%
   filter(hasGeom == "Umzeichnung vorhanden") %>%
-  select(-hasGeom, -Art)
+  select(-hasGeom)
 
 #colnames(Miletus_geom)
 
@@ -137,20 +145,20 @@ buildings_complete <- buildings_dating_dupl %>%
 # remove "." from colnames so css will work 
 colnames(buildings_complete) <- gsub("\\.", "_", colnames(buildings_complete))
 
-
+mapversion <- "v3-3"
 
 # export and save a geojson again (remember to change date for new version)
-filename <- "export/20230127_Miletus_geom"
-#file.remove(filename)
-st_write(buildings_complete, precision = 10, 
-         dsn = paste(filename, ".geojson", sep = ""), 
-         delete_dsn = TRUE)
+filename <- paste0("SHP/export/Map_of_Miletus_", mapversion, "_complete")
+#file.remove(paste0(filename, ".shp"))
 st_write(buildings_complete, precision = 10, 
          layer_options = "ENCODING=UTF-8", 
          dsn = paste(filename, ".shp", sep = ""), 
          driver="ESRI Shapefile", 
          delete_dsn = TRUE)
-
+#' if you get the error: "GDAL Error 1: SHP/export/Map_of_Miletus_v3-2_complete.shp does not appear to be a file or directory."
+#' you just need to replace the line 'delete_dsn = TRUE' with 'delete_dsn = FALSE'
+#' because it cannot replace/delete/overwrite a file that does not exist yet
+#' this will always happen if you change the version number for the first time
 
 # for export of everything separated by period-group:
 
@@ -165,33 +173,37 @@ for (group in unique(periods_groups$group)) {
   group_list <- append(group_list, new_list)
 }
 # add rows without dating
-group_list <- append(group_list, list("Undatiert" = buildings_complete[which(is.na(buildings_complete$group)),]))
+groupless_ind <- which(is.na(buildings_complete$group))
+group_list <- append(group_list, 
+    list("Undatiert" = buildings_complete[groupless_ind,]))
 
 # save each group as an individual file: 
 for (list in seq_along(group_list)) {
   # remove bad characters from the name
+  print(filename)
   filename <- gsub(" \\/ ", "_", names(group_list[list]))
+  print(filename)
   # fixing this manually because i cant grep the ş
   if (grepl("emiratszeitlich", filename)) {
-    filename <- "Mentesche_emiratszeitlich"
+    todo_name <- "Mentesche_emiratszeitlich"
+  } else {
+    todo_name <- gsub(" \\/ ", "_", names(group_list[list]))
   }
+  print(todo_name)
   # add a 0 before single numbers so that it will be sorted correctly in explorer etc.
   num <- ifelse(list < 10, paste("0", list, sep = ""), list)
-  # paste together the filename from number and list name etc.
-  filename_geojson <- paste("export/single/Map_of_Miletus_v1-1_", num, "_", filename, ".geojson", sep = "")
+  filename <- paste0("SHP/export/periods/Map_of_Miletus_", 
+                     mapversion, "_", num, "_", 
+                     todo_name, ".shp", sep = "")
   
-  print(filename_geojson)
-  # delete_dsn = TRUE removes previous files!
-  # save as individual files!
-  st_write(group_list[[list]], precision = 10, dsn = filename_geojson, delete_dsn = TRUE)
-  
-  
-  filename_shp <- paste("export/single/Map_of_Miletus_v1-1_", num, "_", filename, ".shp", sep = "")
-  
-  print(filename_shp)
+  print(filename)
   # delete_dsn = TRUE removes previous files!
   # save as individual files!
   if (nrow(group_list[[2]]) > 0) {
-    st_write(group_list[[list]], precision = 10, layer_options = "ENCODING=UTF-8", dsn = filename_shp, driver="ESRI Shapefile", delete_dsn = TRUE)
+    st_write(group_list[[list]], precision = 10, 
+             layer_options = "ENCODING=UTF-8", 
+             dsn = filename, driver="ESRI Shapefile", 
+             delete_dsn = TRUE)
   }
 }
+
--- R/load_data.R
+++ R/load_data.R
@@ -1,28 +1,31 @@
 library(idaifieldR)
-## this will work only from idaifieldR 0.2.2!
-options(digits = 20)
-connection <- connect_idaifield(serverip = "127.0.0.1",
-                                user = "R",
-                                pwd = "hallo")
+library(dplyr)
 
-buildings_raw <- get_idaifield_docs(
-  connection = connection,
-  projectname = "milet", raw = FALSE) %>%
-  select_by(by = "liesWithin", value = "Bauwerkskatalog")
+## last used with idaifieldR 0.3.5!
+
+options(digits = 20)
+connection <- connect_idaifield(pwd = "hallo", 
+                                project = "milet")
+
+index <- get_field_index(connection)
+
+bwkat_id <- index %>% filter(identifier == "Bauwerkskatalog") %>% pull(UID)
+buildings_raw <- idf_query(connection, 
+                           field = "relations.liesWithin", 
+                           value = bwkat_id) %>%
+  check_and_unnest()
 
 buildings <- buildings_raw %>%
-  simplify_idaifield(replace_uids = TRUE, keep_geometry = TRUE)
+  simplify_idaifield(replace_uids = TRUE, keep_geometry = TRUE, uidlist = index)
 
 data_mat <- buildings %>% idaifield_as_matrix() %>% as.data.frame()
 rownames(data_mat) <- data_mat[,"identifier"]
 
-data_mat$identifier[which(is.na(data_mat$geometry))]
-
-periods_dat <- read.csv(file = "import/period_dat.csv", sep = ",", encoding = "UTF-8")
+periods_dat <- read.csv(file = "R/data/period_dat.csv", sep = ",", encoding = "UTF-8")
 periods_dat <- periods_dat[,-1]
 periods_dat <- periods_dat[!is.na(periods_dat$period),]
 periods_dat$period <- factor(periods_dat$period, levels = periods_dat$period, labels = periods_dat$period, ordered = TRUE)
 
-periods_groups <- read.csv(file = "import/period_cleanGroups.csv", sep = ",", encoding = "UTF-8")
+periods_groups <- read.csv(file = "R/data/period_cleanGroups.csv", sep = ",", encoding = "UTF-8")
 periods_groups$group <- factor(periods_groups$group, levels = periods_groups$group, labels = periods_groups$group, ordered = TRUE)
 
